60dd2efa7593d1bbca5568fe5bbd1c0452d05674
dkarwot <damian.karwot@gmail.com>
2017-07-26 18:46:10 +0200

    upd

--- countBs.js
+++ countBs.js
@@ -1,8 +1,8 @@
 function countBs(string){
     var counter = 0;
-        for(i=0; i<string.lenght; i++)
+        for(i=0; i<string.lenght; i++){
             if (string.charAt(i) == "B") 
-            counter += 1;
+            counter += 1;}
         return counter;
 }
 console.log(countBs("BBB"));
