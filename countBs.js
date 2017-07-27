055fc35ce55240d78bee8614ad8aa76851f72eed
dkarwot <damian.karwot@gmail.com>
2017-07-27 12:27:50 +0200

    changed to table

--- countBs.js
+++ countBs.js
@@ -1,7 +1,7 @@
 function countBs(string){
     var counter = 0;
         for(i=0; i<string.length; i++){
-            if (string.charAt(i) == "B") 
+            if (string[i] == "B") 
             counter += 1};
         return counter;
 }
