57ae29aa194589e108a96ff9a680d3297c845be6
dkarwot <30319287+dkarwot@users.noreply.github.com>
2017-07-27 09:28:50 +0200

    typo update

--- countBs.js
+++ countBs.js
@@ -1,6 +1,6 @@
 function countBs(string){
     var counter = 0;
-        for(i=0; i<string.lenght; i++){
+        for(i=0; i<string.length; i++){
             if (string.charAt(i) == "B") 
             counter += 1;}
         return counter;
