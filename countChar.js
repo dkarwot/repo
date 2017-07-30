ada155fc0dbef3353909f32df4a33c146a93ad65
dkarwot <damian.karwot@gmail.com>
2017-07-30 21:44:00 +0200

    var to let

--- countChar.js
+++ countChar.js
@@ -1,7 +1,7 @@
 "use strict";
 function countChar(string, letter){
-    var counter = 0;
-        for(var i=0; i<string.length; i++){
+    let counter = 0;
+        for(let i=0; i<string.length; i++){
             if (string[i] == letter) 
             counter += 1};
         return counter;
